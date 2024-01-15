c9972f9e537bddc12e6b99aa84914e2171f3c10f
Rantonen Emilia Suvi Katariina <emilia.s.rantonen@student.jyu.fi>
2024-01-15 11:14:27 +0200

    4.5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -10,7 +10,16 @@ const totalLikes = (blogs) => {
     
 }
 
+const favoriteBlog = (blogs) => {
+    const blogWithMaxLikes = blogs.reduce((maxBlog, currentBlog) => {
+        return currentBlog.likes > maxBlog.likes ? currentBlog : maxBlog;
+      }, blogs[0])
+      
+      return blogWithMaxLikes
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
