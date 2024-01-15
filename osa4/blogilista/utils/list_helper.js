5fe5c73c917ac09364a805acb4e37dbd5eb9cc4c
Rantonen Emilia Suvi Katariina <emilia.s.rantonen@student.jyu.fi>
2024-01-15 19:24:30 +0200

    4.6&4.7

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,25 +1,66 @@
-const blog = require('../models/blog')
-
 const dummy = (blogs) => {
-    return 1
+  return 1
 }
   
 const totalLikes = (blogs) => {
-    const reducer = ((sum, blog) => sum + blog.likes)
-    return blogs.reduce(reducer, 0)
+  const reducer = ((sum, blog) => sum + blog.likes)
+  return blogs.reduce(reducer, 0)
     
 }
 
 const favoriteBlog = (blogs) => {
-    const blogWithMaxLikes = blogs.reduce((maxBlog, currentBlog) => {
-        return currentBlog.likes > maxBlog.likes ? currentBlog : maxBlog;
-      }, blogs[0])
-      
-      return blogWithMaxLikes
+  const blogWithMaxLikes = blogs.reduce((maxBlog, currentBlog) => {
+    return currentBlog.likes > maxBlog.likes ? currentBlog : maxBlog
+  }, blogs[0])
+  return blogWithMaxLikes
 }
 
+const mostBlogs = (blogs) => {
+  const theAuthor = {
+    author: '',
+    blogs: 0
+  }
+
+  const authors = blogs.reduce((acc, blog) => {
+    acc[blog.author] = (acc[blog.author] || 0) + 1
+    return acc
+  }, {})
+
+  const [author, blogCount] = Object.entries(authors).reduce((max, entry) => (entry[1] > max[1] ? entry : max), ['', 0])
+
+  theAuthor.author = author
+  theAuthor.blogs = blogCount
+
+  return theAuthor
+}
+
+const mostLikes = (blogs) => {
+  const theAuthor = {
+    author: '',
+    likes: 0
+  }
+
+  const authors = blogs.reduce((acc, blog) => {
+    acc[blog.author] = (acc[blog.author] || 0) + blog.likes
+    return acc
+  }, {})
+
+  const [author, totalLikes] = Object.entries(authors).reduce(
+    (max, entry) => (entry[1] > max[1] ? entry : max),
+    ['', 0]
+  )
+
+  theAuthor.author = author
+  theAuthor.likes = totalLikes
+
+  return theAuthor
+}
+
+
 module.exports = {
-    dummy,
-    totalLikes,
-    favoriteBlog
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
