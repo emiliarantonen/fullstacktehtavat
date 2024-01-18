d43207d3842b6564ec211436e7259a089829d857
Rantonen Emilia Suvi Katariina <emilia.s.rantonen@student.jyu.fi>
2024-01-18 11:14:47 +0200

    4.10-4.12

--- osa4/blogilista/controllers/blog.js
+++ osa4/blogilista/controllers/blog.js
@@ -9,9 +9,23 @@ blogRouter.get('/', (request, response) => {
       response.json(blogs)
     })
 })
+
+blogRouter.get('/:id', async (request, response)=>{
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
+})
   
 blogRouter.post('/', (request, response) => {
   const blog = new Blog(request.body)
+
+
+  if (!(request.body.title) || !(request.body.url)) {
+    return response.status(400).json({ error: 'Title and url are required' })
+  }
 
   blog
     .save()
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -11,19 +11,88 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
-test('there are two blogs', async () => {
-  const response = await api.get('/api/blogs')
+// test('there are two blogs', async () => {
+//   const response = await api.get('/api/blogs')
   
-  expect(response.body).toHaveLength(2)
-})
+//   expect(response.body).toHaveLength(2)
+// })
 
 test('id is defined', async ()=>{
   const response = await api.get('/api/blogs')
-  console.log(response.body)
 
   response.body.forEach(blog => {
     expect(blog.id).toBeDefined()
   })
+})
+
+test('blog can be added', async () =>{
+  const newBlog = {
+    title: 'testi',
+    author: 'test',
+    url: '22222',
+    likes: 5
+  }
+
+  const initialResponse = await api.get('/api/blogs')
+  const initialLength = initialResponse.body.length
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(initialLength +1)
+
+  const addedBlog = response.body.find(blog => blog.title === 'testi')
+  expect(addedBlog).toBeDefined()
+
+})
+
+test('likes set to 0 if not provided', async() => {
+  const newBlog = {
+    title: 'likes test',
+    author: 'test',
+    url: '22222'
+  }
+
+  const response = 
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+  
+  const addedBlog = await api.get(`/api/blogs/${response.body.id}`)
+  console.log(addedBlog.body)
+  expect(addedBlog.body.likes).toBe(0)
+
+})
+
+test('title missing', async()=>{
+  const newBlog = {
+    author: 'test',
+    url: '22222'
+  }
+ 
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+})
+
+test('url missing', async()=>{
+  const newBlog = {
+    title: 'url',
+    author: 'test'
+  }
+ 
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
 })
 
 afterAll(async () => {
