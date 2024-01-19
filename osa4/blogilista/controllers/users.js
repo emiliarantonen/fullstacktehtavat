b4c8ad6cf97d15d93dd450eedf672d49ea1f80ba
Rantonen Emilia Suvi Katariina <emilia.s.rantonen@student.jyu.fi>
2024-01-19 16:56:36 +0200

    4.16

--- osa4/blogilista/controllers/users.js
+++ osa4/blogilista/controllers/users.js
@@ -10,6 +10,18 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+
+  const existingUser = await User.findOne({ username })
+  if (existingUser) {
+    return response.status(400).json({
+      error: 'Username must be unique'
+    })
+  }
+
+  if (!username || !password || username.length < 3 || password.length < 3) {
+    return response.status(400).json({ error: 'Username and password must be at least 3 characters long.' })
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const helper = require('../utils/list_helper')
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
 const api = supertest(app)
 
@@ -128,6 +130,77 @@ test('blog can be updated', async()=>{
 
   expect(updatedBlog.likes).toBe(updatedLikes)
 
+})
+
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+})
+
+
+describe('user validation', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+  test('username and password length', async()=>{
+    const invalidUser = {
+      username: 'username123',
+      password: 'pw',
+    }
+
+    const response = await api.post('/api/users').send(invalidUser).expect(400)
+
+    expect(response.body.error).toContain('Username and password must be at least 3 characters long')
+  })
+
+  test('non-unique username', async () => {
+    const duplicateUser = {
+      username: 'root',
+      password: 'anotherpassword',
+    }
+
+    const response = await api
+      .post('/api/users')
+      .send(duplicateUser)
+      .expect(400)
+
+
+    expect(response.body.error).toContain('Username must be unique')
+  
+  })
 })
 
 afterAll(async () => {
